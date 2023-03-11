cdaaa1e40a0335c244e206ff15416eca90ca8879
Asit Biswas <asitbiswas870@gmail.com>
2023-03-11 20:16:50 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
+import App from './components/App';
 
+const router = createBrowserRouter([{
+  path:"/",
+  element : <App/>
+},{
+  path:"/add-question/",
+  element : <div>add-question</div>
+},{
+  path:"/add-answer/",
+  element : <div>add-answer</div>
+},{
+  path:"/login",
+  element : <div>login</div>
+}
+,{
+  path:"/register/",
+  element : <div>register</div>
+}
+])
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
