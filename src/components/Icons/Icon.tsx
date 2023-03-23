5aede3fef1742000dcc7fbde124476722518917b
Asit Biswas <asitbiswas870@gmail.com>
2023-03-23 21:49:09 +0530

    add error icon

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -477,3 +477,6 @@ export function GoogleIcon(){
 export function FaceBookIcon(){
   return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="icon_svg-fill_as_stroke" fillRule="evenodd" clipRule="evenodd" d="M20.5 12a8.5 8.5 0 1 0-9.83 8.397v-5.94H8.513v-2.458h2.159v-1.872c0-2.13 1.269-3.307 3.21-3.307.93 0 1.903.166 1.903.166v2.091h-1.072c-1.056 0-1.385.656-1.385 1.328V12h2.358l-.377 2.457h-1.98v5.94A8.503 8.503 0 0 0 20.5 12Z" fill="#1877F2"></path></svg>
 }
+export function ErrorIcon(){
+  return <svg width="24px" height="24px" viewBox="0 0 24 24"><g id="error" stroke="none" fill="none" fillRule="evenodd" transform="translate(3.000000, 3.000000)"><path d="M9,12 C9.55228475,12 10,12.4477153 10,13 C10,13.5522847 9.55228475,14 9,14 C8.44771525,14 8,13.5522847 8,13 C8,12.4477153 8.44771525,12 9,12 Z" className="icon_svg-fill_as_stroke" fill="#666666" fillRule="nonzero"></path><path d="M9,9.5 L9,4.5 L9,9.5 Z M9,18 C4.029,18 0,13.971 0,9 C0,4.029 4.029,0 9,0 C13.971,0 18,4.029 18,9 C18,13.971 13.971,18 9,18 Z" className="icon_svg-stroke" stroke="#666" strokeWidth="1.5" strokeLinecap="round"></path></g></svg>
+}
