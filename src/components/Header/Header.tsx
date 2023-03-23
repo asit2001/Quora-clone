d660355159c9acbaa69d462cc9555e03f1fb75ba
Asit Biswas <asitbiswas870@gmail.com>
2023-03-23 21:48:20 +0530

    change location to useLocation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,23 @@
 import { useEffect } from "react";
-import "./styles/Header.style.css";
+import {useLocation} from 'react-router-dom'
 import { useAppSelector, useAppDispatch, setSearch } from "../../redux";
-import { userType } from "../../types";
 
+import { userType } from "../../types";
 import SearchQuestions from "./SearchQuestions";
 import LoggedIn from "./LoggedIn";
 import NotLoggedIn from "./NotLoggedIn";
 
+import "./styles/Header.style.css";
+
 const Header: React.FC<{userInfo:userType}> = ({userInfo}) => {
-  const pathname = window.location.pathname;
+  const {pathname} = useLocation()
+  
   const dispatch = useAppDispatch();
   const search = useAppSelector((state) => state.search.value);
 
   useEffect(() => {
     dispatch(setSearch(""));
-  }, [pathname]);
+  }, [pathname,dispatch]);
   return (
     <header className={search.length ? "header" : ""}>
       <nav className="nav active">
