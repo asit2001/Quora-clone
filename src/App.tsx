b011486d090251a0794f3e7af800e7d362a2e535
Asit Biswas <asitbiswas870@gmail.com>
2023-03-24 21:26:25 +0530

    add login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import LogIn from './components/LogIn/LogIn';
 import Home from './pages/Home'
 const router = createBrowserRouter([
   {
     path:"/",
     element:<Home/>
+  },{
+    path:"/login",
+    element:<LogIn/>
   }
 ]);
 export default function App() {
