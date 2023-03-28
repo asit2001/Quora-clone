1240da05f3009ce66d248e1fa6805e10b7cfd17f
Asit Biswas <asitbiswas870@gmail.com>
2023-03-28 19:37:09 +0530

    remove old files

--- src/utils/toastError.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify"
-
-export const showErrorToast = (reason,position="top-right")=>{
-    toast.error(reason,{
-        position: position,
-        autoClose: 3000,
-        theme: "light",
-    })
-}
