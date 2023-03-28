bf30ddacac2af7e048a5d47ea9f08954cbbc69a9
Asit Biswas <asitbiswas870@gmail.com>
2023-03-28 18:46:35 +0530

    add route for ans page and question page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import LogIn from './pages/LogIn';
 import Home from './pages/Home'
+import Answer from './pages/Answer';
+import Question from './pages/Question';
 const router = createBrowserRouter([
   {
     path:"/",
@@ -8,6 +10,12 @@ const router = createBrowserRouter([
   },{
     path:"/login",
     element:<LogIn/>
+  },{
+    path:"/answer",
+    element:<Answer/>
+  },{
+    path:"/question/:id",
+    element:<Question/>
   }
 ]);
 export default function App() {
