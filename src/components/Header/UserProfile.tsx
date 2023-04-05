8ddcd0b68ee5cb24ab277684dd2dea075ea1b457
Asit Biswas <asitbiswas870@gmail.com>
2023-04-05 18:04:10 +0530

    added logout func

--- src/components/Header/UserProfile.tsx
+++ src/components/Header/UserProfile.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { setShowQns, useAppDispatch } from "../../redux";
+import { Tooltip } from "react-tooltip";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile: React.FC<{ name: string }> = ({ name }) => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  function logOut() {
+    localStorage.removeItem("user")
+    navigate("/login");
+  }
   return (
     <>
       <img
         src={`https://i.pravatar.cc/150?u=${name}`}
         alt="user avatar"
         className="avatar"
+        data-tooltip-id="logout"
       />
+      <Tooltip 
+        id="logout"
+        openOnClick={true}
+        clickable={true}
+        className="tooltip__model"
+        place="bottom"
+      >
+        <button onClick={logOut} className="tooltip__btn">Logout</button>
+      </Tooltip>
       <button className="btn addQuestion" onClick={()=>{dispatch(setShowQns(true))}}>
         Add Questions
       </button>
