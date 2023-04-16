96fe2993c265b4756cdab35804b3f70b6e8cd546
Asit Biswas <asitbiswas870@gmail.com>
2023-04-17 02:17:52 +0530

    change image link

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -12,7 +12,7 @@ function SideBar() {
         <Link to={"/question/What-is-the-answer-to-30401234"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-951-100-N3P1sk6vsq72as9qqwA30zUMvxgBPWJ1.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FMathematics.jpeg?alt=media"
             alt="Math"
           />
           <p>Mathematics</p>
@@ -20,7 +20,7 @@ function SideBar() {
         <Link to={"/question/How-has-science-proven-gravity"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-931-100-c8WCPwZ9qPsh5zLGQ5wHh1ddxtc9Cch7.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FScience.jpeg?alt=media"
             alt="science"
           />
           <p>Science</p>
@@ -28,7 +28,7 @@ function SideBar() {
         <Link to={"/question/How-do-inventors-innovate-large-bulky-technology-and-make-it-smaller-and-more-effective"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-2177-100-JiR07D1TQSfeQzRvWXomVaY4Poj2f8Yb.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FTechnology.jpeg?alt=media"
             alt="Technology"
           />
           <p>Technology</p>
@@ -36,7 +36,7 @@ function SideBar() {
         <Link to={"/question/What-are-some-interesting-examples-of-photographs-that-show-important-historical-figures-in-unexpected-places-or-companies"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-1393081-100-ehjlsiwapdqhrwojxumsowmqctapbgfx.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FHistorical.jpeg?alt=media"
             alt="Historical"
           />
           <p>Historical</p>
@@ -44,7 +44,7 @@ function SideBar() {
         <Link to={"/question/Do-you-think-that-every-aborted-fetus-lives-on-in-the-afterlife-Are-you-worried-about-where-unborn-fetuses-go-after-they-die-Is-this-why-you-oppose-abortion"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-236216-100-xjbypdjmbdpsbsspidupljqjwarbnswz.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FFetuses.jpeg?alt=media"
             alt="Fetuses"
           />
           <p>Fetuses</p>
@@ -52,7 +52,7 @@ function SideBar() {
         <Link to={"/question/Who-would-win-in-a-3v1-Luffy-Naruto-and-Ichigo-at-their-strongest-or-Goku-in-the-Saiyan-Saga"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-3711-100-wkcgllpjwevqcbkmvoemfnvnbpkltyzb.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FAnime.jpeg?alt=media"
             alt="Anime"
           />
           <p>Anime</p>
@@ -60,7 +60,7 @@ function SideBar() {
         <Link to={"/question/Did-Hinata-ever-have-a-crush-on-Naruto-when-they-were-young"} className="upperPanel__bar">
           <img
             className="upperPanel__bar__icon"
-            src="https://qph.cf2.quoracdn.net/main-thumb-t-438778-100-zterdpriqklxsvmoclnnjudspgerddek.jpeg"
+            src="https://firebasestorage.googleapis.com/v0/b/quora-clone-asit.appspot.com/o/public%2FNaruto.jpeg?alt=media"
             alt="Naruto"
           />
           <p>Naruto</p>
